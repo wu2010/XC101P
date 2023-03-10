8a9c81852466eb40f2a56bb00a59d67961d497f9
Yongning <wu2010@gmail.com>
2023-03-09 23:20:05 -0800

    set up a project

---
+++ chinese_remainder.py
@@ -0,0 +1,8 @@
+# First they tried to form 4 rows but 3 soldiers are left out;
+# then they tried to line up in 5 rows and 4 soldiers were left out.
+# Finally, they formed x rows without any extra.
+# What is the minimum number of these soldiers?
+
+x = int(input())
+
+print(19)
